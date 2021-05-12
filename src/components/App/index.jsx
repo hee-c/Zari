bc4b95e28dd332c69c5961320713d05d97fcb128
hee-c <ways2kim@gmail.com>
2021-05-12 20:43:29 +0900

    Fix image loading sequence

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import Home from '../Home';
 import WaitingArea from '../WaitingArea';
-import Room from '../Room';
+import Room from '../../pages/Room';
+import { imageLoader } from '../../pixi';
 
 export default function App() {
+  useEffect(() => {
+    imageLoader();
+  }, []);
+
   return (
     <Router>
       <Switch>
